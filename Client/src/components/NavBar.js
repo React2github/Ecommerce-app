02de588dbc2a0c922839d122ff53d793cc06c036
Abdella <abdella.shukur2@gmail.com>
2020-07-22 18:38:18 -0400

    Initial Commit

---
+++ Client/src/components/NavBar.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import "./Home.css";
+// import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../actions/userActions";
+// import { useSelector } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+} from "react-router-dom";
+import {
+  FaShoppingCart,
+  FaEnvelope,
+  FaUserCheck,
+  FaCogs,
+  FaHome,
+  FaKey,
+  FaSignOutAlt
+} from "react-icons/fa";
+import Footer from "./Footer";
+
+function NavBar() {
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logout());
+    history.push("/");
+  };
+
+        return (
+            <Route>
+                <div>
+                    <div id="navlinks">
+                        <nav id="nav" className="navbar">
+                        <Link to='/'>Home
+                        <FaHome id='contacticon' color='rgb(225, 203, 78' size={37} />
+                        </Link>
+                            
+                            <Link to='/submission'>Submission
+                             <FaEnvelope  id='contacticon' color='rgb(225, 203, 78' size={37} />
+                            </Link>
+
+            <Link to="/api/products">
+              Products
+              <FaCogs id="contacticon" color="rgb(225, 203, 78" size={37} />
+            </Link>
+            {userInfo ? (
+              <Link to="/cart/:id?">
+                Cart
+                <FaShoppingCart
+                  id="carticon"
+                  color="rgb(225, 203, 78"
+                  size={37}
+                />
+                <i className="fas fa-shopping-cart"></i>
+              </Link>
+            ) : (
+              ""
+            )}
+            {userInfo ? (
+              <Link to="/profile">
+                {userInfo.name}
+                <FaUserCheck
+                  id="contacticon"
+                  color="rgb(225, 203, 78"
+                  size={37}
+                />
+              </Link>
+            ) : (
+              <Link to="/signin">
+                Sign In
+                <FaKey id="contacticon" color="yellow" size={37} />
+              </Link>
+            )}
+
+            {userInfo ? (
+              <Link onClick={handleLogout}>
+                logout
+                <FaSignOutAlt
+                  id="contacticon"
+                  color="rgb(225, 203, 78"
+                  size={37}
+                />
+              </Link>
+            ) : (
+              ""
+            )}
+            <span></span>
+          </nav>
+        </div>
+      </div>
+    </Route>
+  );
+}
+
+export default NavBar;
