06f6865af605a06196f98750a25e4cd441c71173
Abdella <abdella.shukur2@gmail.com>
2020-07-23 12:03:08 -0400

    removed extra line

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,7 +1,7 @@
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 var config = {
-    apiUrl: proxyurl + 'https://enigmatic-ravine-46914.herokuapp.com/api/products' || 'https:localhost:3001'
+    apiUrl: proxyurl + 'https://enigmatic-ravine-46914.herokuapp.com' || 'https:localhost:3001'
   }
 
   module.exports = config
